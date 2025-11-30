8a5fe28aebf194ba9e50b6e2d886f7d1bb18b2f9
lgolikova <milagoul@gmail.com>
2025-11-30 17:27:54 +0300

    Добавлены приватные маршруты

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -8,7 +8,13 @@ import RegisterPage from "./pages/RegisterPage";
 import SpendAnalysisPage from "./pages/SpendAnalysisPage";
 import { transactions } from "./data";
 import { useState } from "react";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
+function PrivateRoute() {
+    const { user } = useContext(AuthContext);
+    return user ? <Outlet /> : <Navigate to="/login" />;
+}
 
 function AppRoutes() {
     const [isSpendSelected, setIsSpendSelected] = useState("");
@@ -20,11 +26,27 @@ function AppRoutes() {
 
     return (
         <Routes>
-            <Route path="/" element={<MainPage transactions={transactions} isSpendSelected={isSpendSelected} onclick={handleSendClick}/>}>
-                <Route path="spend/new" element={<NewSpendPage />} />
-                <Route path="spend/:id" element={<EditSpendPage isSpendSelected={isSpendSelected}/>} />
+            <Route element={<PrivateRoute />}>
+                <Route
+                    path="/"
+                    element={
+                        <MainPage
+                            transactions={transactions}
+                            isSpendSelected={isSpendSelected}
+                            onclick={handleSendClick}
+                        />
+                    }
+                >
+                    <Route path="spend/new" element={<NewSpendPage />} />
+                    <Route
+                        path="spend/:id"
+                        element={
+                            <EditSpendPage isSpendSelected={isSpendSelected} />
+                        }
+                    />
+                </Route>
+                <Route path="/spend-analysis" element={<SpendAnalysisPage />} />
             </Route>
-            <Route path="/spend-analysis" element={<SpendAnalysisPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route path="*" element={<NotFoundPage />} />
